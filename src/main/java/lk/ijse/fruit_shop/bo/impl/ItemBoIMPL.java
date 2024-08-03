979eb0955305148df2028b6873ff83768c994050
madhuille11 <madhushiillesinghe225@gmail.com>
2024-08-03 12:17:11 +0530

    ItemBoImpl create

--- src/main/java/lk/ijse/fruit_shop/bo/impl/ItemBoIMPL.java
+++ src/main/java/lk/ijse/fruit_shop/bo/impl/ItemBoIMPL.java
@@ -1,6 +1,8 @@
 package lk.ijse.fruit_shop.bo.impl;
 
 import lk.ijse.fruit_shop.bo.ItemBo;
+import lk.ijse.fruit_shop.dao.impl.CustomerDaoIMPL;
+import lk.ijse.fruit_shop.dao.impl.ItemDaoIMPL;
 import lk.ijse.fruit_shop.dto.ItemDto;
 
 import java.sql.Connection;
@@ -9,21 +11,25 @@ import java.sql.SQLException;
 public class ItemBoIMPL implements ItemBo {
     @Override
     public String saveItem(ItemDto itemDto, Connection connection) throws SQLException {
-        return null;
+        var itemDaoIMPL = new ItemDaoIMPL();
+        return itemDaoIMPL.saveItem(itemDto, connection);
     }
 
     @Override
     public boolean updateItem(String code, ItemDto itemDto, Connection connection) throws SQLException {
-        return false;
+        var itemDaoIMPL = new ItemDaoIMPL();
+        return itemDaoIMPL.updateItem(code, itemDto, connection);
     }
 
     @Override
     public boolean deleteItem(String code, Connection connection) throws SQLException {
-        return false;
+        var itemDaoIMPL = new ItemDaoIMPL();
+        return itemDaoIMPL.deleteItem(code, connection);
     }
 
     @Override
     public ItemDto getItem(String code, Connection connection) throws SQLException {
-        return null;
+        var itemDaoIMPL = new ItemDaoIMPL();
+        return itemDaoIMPL.getItem(code, connection);
     }
 }
