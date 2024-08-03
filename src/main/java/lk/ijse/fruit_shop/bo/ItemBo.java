9f96b2d34c025acaa67af5c08be3fc458c18a166
madhuille11 <madhushiillesinghe225@gmail.com>
2024-08-03 12:01:46 +0530

    ItemBo create

--- src/main/java/lk/ijse/fruit_shop/bo/ItemBo.java
+++ src/main/java/lk/ijse/fruit_shop/bo/ItemBo.java
@@ -1,4 +1,14 @@
 package lk.ijse.fruit_shop.bo;
 
+import lk.ijse.fruit_shop.dto.CustomerDto;
+import lk.ijse.fruit_shop.dto.ItemDto;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public interface ItemBo {
+    String saveItem(ItemDto itemDto, Connection connection) throws SQLException;
+    boolean updateItem(String code,ItemDto itemDto,Connection connection) throws SQLException;
+    boolean deleteItem(String code,Connection connection) throws SQLException;
+    ItemDto getItem(String code,Connection connection) throws SQLException;
 }
